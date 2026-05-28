fb2917045d865bf9f7d842ee749b29879d024be5
umerfarooq-sde <muhammadumerfarooq.id@gmail.com>
2026-05-29 00:20:28 +0500

    db connection updation

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,21 +1,27 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-// Connect to XAMPP MySQL
-db.connect((err) => {
+// Test Connection
+db.getConnection((err, connection) => {
     if (err) {
         console.error("MySQL Connection Failed:");
         console.error(err.message);
     } else {
         console.log("MySQL Connected Successfully (XAMPP)");
+
+        connection.release();
     }
 });
 
-module.exports = db;
+module.exports = db.promise();
